af47a1357b326bc62187746f642451e190e6ab28
Dawid Dominiak <poczta@dawiddominiak.pl>
2017-01-23 13:13:32 +0100

    ProductPojo, FilePojo added

--- src/module/shopContainer.ts
+++ src/module/shopContainer.ts
@@ -8,13 +8,6 @@ import { sequelizeConfig } from "./../persistance/config";
 import { Persistance } from "./Persistance";
 import { TYPES } from "./types";
 
-const databaseHost = process.env.DATABASE_HOST || "localhost";
-const databasePort = process.env.DATABASE_PORT || 5432;
-const databaseName = process.env.DATABASE_NAME || "postgres";
-const databaseUser = process.env.DATABASE_USER || "postgres";
-const databasePassword = process.env.DATABASE_PASSWORD || "postgres";
-const databaseDialect = process.env.DATABASE_DIALECT || "postgres";
-
 const shopContainer = new Container();
 
 shopContainer.bind<Persistance>(TYPES.Persistance).to(Persistance);
---
+++ src/persistance/pojo/IProductPojo.ts
@@ -0,0 +1,6 @@
+export interface IProductPojo {
+    uuid?: string;
+    name?: string;
+    price?: number;
+    description?: string;
+}
